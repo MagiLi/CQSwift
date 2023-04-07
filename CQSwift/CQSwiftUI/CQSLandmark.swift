1e8ce0b2a777c9869b269ea0042e31f118c714ae
lichaoqun <976906595@qq.com>
2023-04-07 17:29:10 +0800

    添加json文件

---
+++ CQSwift/CQSwiftUI/CQSLandmark.swift
@@ -0,0 +1,13 @@
+//
+//  CQSLandmark.swift
+//  CQSwift
+//
+//  Created by llbt2019 on 2023/4/7.
+//  Copyright © 2023 李超群. All rights reserved.
+//
+
+import Foundation
+
+struct CQSLandmark: Hashable, Codable {
+    
+}
